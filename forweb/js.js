2f6032f09cdf454e6f307b3d43abb7dfad578614
zLiGhTKz <91182890+zLiGhTKz@users.noreply.github.com>
2023-04-23 17:09:27 +0800

    Update js.js

--- forweb/js.js
+++ forweb/js.js
@@ -1,10 +1,7 @@
-// 选择表单元素
 const form = document.querySelector('form');
-
-// 监听表单提交事件
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const username = form.elements.username.value;
   const password = form.elements.password.value;
-  console.log(`用户名：${username}，密码：${password}`);
+  console.log(`用戶名：${username}，密碼：${password}`);
 });
